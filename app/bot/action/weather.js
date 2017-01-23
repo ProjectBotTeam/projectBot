2a1fd0ff44cf712ebb41d6bf937621b8076df79a
Walley Yang <walleyyang@gmail.com>
2017-01-22 22:18:16 -0500

    Initial test setup for weather

--- app/bot/action/weather.js
+++ app/bot/action/weather.js
@@ -1,5 +1,20 @@
-var exports = module.exports = {}
+require('dotenv').config()
 
-exports.getWeather = function () {
-    console.log("The weather is 110 degrees!")
+var exports = module.exports = {}
+var link = 'http://api.wunderground.com/api/' + process.env.WUNDERGROUND_TOKEN + '/conditions/q/'
+var locationPattern = '((\sfor\s)|(\sin\s))([a-zA-Z]+[\s]|[a-zA-Z])+[,][\s][a-zA-Z]+'
+var jsonFile, city, state
+
+exports.getCityState = function(inputMessage) {
+  // TODO Get the city and state from the input message.
+  //console.log('CHECKING...')
+  //console.log(inputMessage.match(locationPattern))
 }
+
+exports.getWeather = function(inputMessage) {
+    // Get city and state
+    jsonFile = link + state + '/' + city + '.json'
+    console.log("The weather is 110 degrees!")
+    console.log(jsonFile)
+}
+
